e88532ce61afb5b3b38ed9c514705b3611f48ce2
henre7860 <henrevandenberg@gmail.com>
2023-08-29 19:35:36 +0200

    some stuff

---
+++ scigrow-report/report-process.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# Input and output file names
+file_name_input = "test-data.xlsx"
+file_name_output = "test-data-output.xlsx"
+
+# Read the input Excel file into a DataFrame
+df = pd.read_excel(file_name_input)
+
+# Define the start and end months for column renaming and processing
+start_month = "1 July 2023"
+end_month = "1 August 2023"
+
+# Get the current column names
+current_columns = df.columns.tolist()
+
+# Create new column names based on column indices and update DataFrame columns
+new_columns = current_columns[:2] + [start_month, end_month] + current_columns[4:]
+df.columns = new_columns
+
+# Process specific columns (including start_month, end_month, and "Total Consumption")
+for column in [start_month, end_month, "Total Consumption"]:
+    # Replace " kWh" and commas, then convert to float
+    new_col = column.replace(' ', '_')
+    df[new_col] = df[column].str.replace(' kWh', '').str.replace(',', '').astype(float)
+
+# Export the cleaned DataFrame to an Excel file
+df.to_excel(file_name_output, index=False)
+
+# Print a success message
+print("Report successfully processed")
